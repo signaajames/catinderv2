24b4c7b71847c4204b8735a72bd89b6d21fbde59
signaajames <penguingnop@>
2026-04-30 15:18:32 +0800

    Added grotessk font locally and added action bars

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,47 @@ import './App.css'
 
 function App() {
 
+  function swipeNope() {
+    console.log("Nope");
+  }
+
+  function swipeLike() {
+    console.log("Like");
+  }
+
+  function swipeSuper() {
+    console.log("Super Like");
+  }
+
   return (
     <>
+    <div className='shell'>
+      <div className='sidebar'>
+        {/* sidebar code */}
+      </div>
 
+      {/* main area */}
+      <div className='main-area'>
+        {/* card area */}
+        <div className='card-area'>
+          <div className='card-stack'>
+            {/* cards inject here */}
+          </div>
+        </div>
+        {/* actionbar area */}
+        <div className="action-bar">
+          <button className="action-btn btn-nope" onClick={swipeNope} title="Nope (←)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+          </button>
+          <button className="action-btn btn-super" onClick={swipeSuper} title="Super Like (↑)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
+          </button>
+          <button className="action-btn btn-like" onClick={swipeLike} title="Like (→)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-heart-icon lucide-heart"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
+          </button>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
