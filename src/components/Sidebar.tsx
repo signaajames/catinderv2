31cf18fdbbd069f6415bb7499d8249317cd9dd70
signaajames <penguingnop@>
2026-04-30 12:20:16 +0800

    inital

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Sidebar() {
+
+  return (
+    <>
+
+    </>
+  )
+}
---
+++ src/components/View.tsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function View() {
+
+  return (
+    <>
+
+    </>
+  )
+}
